ef75993263c4b47691994f477434b66efe6ba038
Denis <chernov-denis21@mail.ru>
2023-12-20 11:51:37 +0300

    changed the main file for the second time in the dev branch

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.print("Это добавлено в ветке dev в первый раз");
+        System.out.print("Это добавлено в ветке dev во второй раз");
     }
 }
